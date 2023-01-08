9c1e4d1d2c227a86612b8944aca628f6519257b1
wan-elmus <elmuswangudi@gmail.com>
2023-01-08 21:33:31 +0300

    it should work now

--- getpdf.sh
+++ getpdf.sh
@@ -41,30 +41,59 @@ rm index.html pdf_links.txt
 pdf_count=$(ls -1 $dir_name | wc -l)
 echo "$pdf_count PDF files have been downloaded to $dir_name."
 
-# Print a tabulated summary of the PDF files
+# Provide a tabulated summary of the downloaded PDF files
 echo "Filename                Size"
 echo "------------------------ -----"
-for file in $dir_
+for file in $dir_name/*; do
+    size=$(stat -c %s "$file")
+    size_in_kb=$(awk "BEGIN {printf \"%.3f\", $size/1024}")
+    size_in_mb=$(awk "BEGIN {printf \"%.3f\", $size/1024/1024}")
+    if [ $size -lt 1024 ]
+    then
+        printf "%-23s %5d bytes\n" $(basename "$file") $size
+    elif [ $size -lt 1048576 ]
+    then
+        printf "%-23s %5.3f kb\n" $(basename "$file") $size_in_kb
+    else
+        printf "%-23s %5.3f mb\n" $(basename "$file") $size_in_mb
+    fi
+done
 
+# Check if the -z option was provided
+if [ $# -eq 1 ] && [ "$1" == "-z" ]
+then
+    # Create a zip archive with the same name as the directory
+    zip -r $dir_name.zip $dir_name
+    # Remove the directory
+    rm -r $dir_name
+    echo "PDF files have been added to a zip archive called $dir_name.zip."
+else
+    # Check if any invalid flags were provided
+    if [ $# -ne 0 ]
+    then
+        echo "Error: Invalid flag provided."
+        exit 3
+    fi
+fi
 
-:'
-The script begins by prompting the user to enter a URL.
+# :'
+# The script begins by prompting the user to enter a URL.
 
-The script checks if the user provided a URL. If no URL was provided, the script displays an error message and exits with an exit code of 1.
+# The script checks if the user provided a URL. If no URL was provided, the script displays an error message and exits with an exit code of 1.
 
-The script downloads the HTML page at the provided URL using the wget command.
+# The script downloads the HTML page at the provided URL using the wget command.
 
-The script extracts all the PDF links from the HTML page using the grep and sed commands, and saves them to a file called pdf_links.txt.
+# The script extracts all the PDF links from the HTML page using the grep and sed commands, and saves them to a file called pdf_links.txt.
 
-The script checks if there are any PDF links in the file. If the file is empty, the script displays a message indicating that no PDF files were found at the provided URL, removes the temporary files index.html and pdf_links.txt, and exits with an exit code of 2.
+# The script checks if there are any PDF links in the file. If the file is empty, the script displays a message indicating that no PDF files were found at the provided URL, removes the temporary files index.html and pdf_links.txt, and exits with an exit code of 2.
 
-The script creates a unique directory to store the PDF files. The directory name is based on the current date and time, and is formatted as pdf_files_YYYYMMDD_HHMMSS.
+# The script creates a unique directory to store the PDF files. The directory name is based on the current date and time, and is formatted as pdf_files_YYYYMMDD_HHMMSS.
 
-The script reads each PDF link from the pdf_links.txt file and downloads the corresponding PDF file using the wget command. The PDF files are saved to the directory created in step 6.
+# The script reads each PDF link from the pdf_links.txt file and downloads the corresponding PDF file using the wget command. The PDF files are saved to the directory created in step 6.
 
-The script removes the temporary files index.html and pdf_links.txt.
+# The script removes the temporary files index.html and pdf_links.txt.
 
-The script counts the number of PDF files in the directory and prints a message indicating how many PDF files were downloaded and the directory name.
+# The script counts the number of PDF files in the directory and prints a message indicating how many PDF files were downloaded and the directory name.
 
-The script prints a tabulated summary
-'
+# The script prints a tabulated summary
+# '

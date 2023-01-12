19c79f67657ac64ba71a26a289828ffad16668b6
wan-elmus <elmuswangudi@gmail.com>
2023-01-12 19:59:37 +0300

    stubborn dude!

--- getpdf8.sh
+++ getpdf8.sh
@@ -3,16 +3,16 @@
 # Full name and student number
 
 # Check if any invalid flags were provided
-for flag in "$@"
-do
-    if [ "$flag" != "-z" ]
-    then
-        tput setaf 1
-        echo "Invalid option error. Only -z for zip PDFs is valid - exiting.."
-        tput sgr0
-        exit 3
-    fi
-done
+OPTERR ="Invalid option error. Only -z for zip file is valid - exiting.."
+
+if [[ $# -gt 0 ]]; then
+    while getopts "z" opt; do
+    case $opt in
+        z) zipit=true;;
+        *) echo -e "$OPTERR" && exit 1;;
+        esac
+    done
+fi
 
 # Check if the user provided a URL
 read -p "Enter a URL: " url
